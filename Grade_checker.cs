61356879a6160b48dd100eb0250d718e666dd7ba
Asanda Zondo <asandazondo884@gmail.com>
2025-08-28 04:15:03 +0200

    Add files via upload

---
+++ Grade_checker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Your Marks:");
+            int Marks = int.Parse(Console.ReadLine());
+
+            if (Marks >= 50)
+                Console.WriteLine("Pass");
+            else
+                Console.WriteLine("Fail");
+            Console.ReadLine();
+
+
+        }
+    }
+}
